86397e836097459118289b20ced23965e20ccd49
laurens_avocadomedia <laurens@avocado-media.nl>
2022-07-01 16:02:24 +0200

    wip: addign template

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,31 @@
 <x-layout>
-@foreach ($posts as $post)
-    <article>
-        <a href="{{ route('posts.show', $post->slug) }}">
-            <h1>{{ $post->title }}</h1>
-        </a>
+{{--@foreach ($posts as $post)--}}
+{{--    <article>--}}
+{{--        <a href="{{ route('posts.show', $post->slug) }}">--}}
+{{--            <h1>{{ $post->title }}</h1>--}}
+{{--        </a>--}}
 
-        By: <a href="{{ route('authors.show', $post->author->username) }}"><p>{{ $post->author->name }}</p></a> <a href="{{ route('categories.show', $post->category->slug) }}"><p>{{ $post->category->name }}</p></a>
+{{--        By: <a href="{{ route('authors.show', $post->author->username) }}"><p>{{ $post->author->name }}</p></a> <a href="{{ route('categories.show', $post->category->slug) }}"><p>{{ $post->category->name }}</p></a>--}}
 
-        <div>
-            {{ $post->excerpt }}
+{{--        <div>--}}
+{{--            {{ $post->excerpt }}--}}
+{{--        </div>--}}
+{{--    </article>--}}
+{{--@endforeach--}}
+    @include('posts._post-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
         </div>
-    </article>
-@endforeach
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+    </main>
 </x-layout>
